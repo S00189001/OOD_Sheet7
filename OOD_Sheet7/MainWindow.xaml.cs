9f6ae1a96f9badc868abc24c595711a4ba0cf3c3
Matthew Bowen <46889006+S00189001@users.noreply.github.com>
2020-03-22 22:31:11 +0000

    Exercise 1 complete

---
+++ OOD_Sheet7/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OOD_Sheet7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        NORTHWNDEntities db = new NORTHWNDEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Exercise 1
+        private void UI_Ex1_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var query = from customer in db.Customers
+                        group customer by customer.Country into customerGroup
+                        orderby customerGroup.Count() descending
+                        select new
+                        {
+                            Country = customerGroup.Key,
+                            Count =  customerGroup.Count()
+                        };
+
+            UI_Ex1_Listbox.ItemsSource = query.ToList();
+        }
+    }
+}
