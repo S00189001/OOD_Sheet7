f9cc01cf5c25c650e7bf04533d3cc990a4648b42
Matthew Bowen <46889006+S00189001@users.noreply.github.com>
2020-03-22 23:02:30 +0000

    Forgot to commit :)

--- OOD_Sheet7/MainWindow.xaml.cs
+++ OOD_Sheet7/MainWindow.xaml.cs
@@ -40,5 +40,57 @@ namespace OOD_Sheet7
 
             UI_Ex1_Listbox.ItemsSource = query.ToList();
         }
+
+        private void UI_Ex2_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var query = from customers in db.Customers
+                        where customers.Country == "Italy"
+                        //group customers by customers.Country into italyCustomers
+                        //orderby italyCustomers.Key.Count()
+                        select customers;
+            //{
+            //    Orders = " ",
+            //    CustomerDemographics = " ",
+            //    CustomerID = customers  
+            //};
+
+            UI_Ex2_Listbox.ItemsSource = query.ToList();
+        }
+
+        private void UI_Ex3_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var query = from products in db.Products
+                        where (products.UnitsInStock - products.UnitsOnOrder > 0)
+                        select new
+                        {
+                            Product = products.ProductName,
+                            Available = products.UnitsInStock - products.UnitsOnOrder
+                        };
+
+            UI_Ex3_Listbox.ItemsSource = query.ToList();
+        }
+
+        private void UI_Ex4_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var query = from disProducts in db.Order_Details
+                        orderby disProducts.Product.ProductName
+                        where disProducts.Discount > 0
+                        select new
+                        {
+                            ProductName = disProducts.Product.ProductName,
+                            DiscountGiven = disProducts.Discount,
+                            OrderID = disProducts.OrderID
+                        };
+
+            UI_Ex4_Listbox.ItemsSource = query.ToList();
+        }
+
+        private void UI_Ex5_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var query = from items in db.Orders
+                        select items.Freight;
+
+            UI_Ex5_TextBlock.Text = string.Format("The total value of freight for all for all orders is {0:C}", query.Sum());
+        }
     }
 }
